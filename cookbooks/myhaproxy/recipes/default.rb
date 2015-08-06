8e068eded45e4e6463c85fd38156c2119bc2a0ec
Najwa Azer <najwa.azer@gmail.com>
2015-08-06 13:02:03 -0400

    Create roles

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -4,11 +4,20 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-  "hostname" => "ec2-54-175-133-183.compute-1.amazonaws.com",
-  "ipaddress" => "54.175.133.183",
-  "port" => 80,
-  "ssl_port" => 80
-}]
+all_web_nodes = search('node', 'role:web')
+members = []
 
-include_recipe "haproxy::default"
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname'  => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port'      => 80,
+    'ssl_port'  => 80
+  }
+  members.push(member)
+end
+
+
+node.default['haproxy']['members'] = members
+
+include_recipe 'haproxy::default'
